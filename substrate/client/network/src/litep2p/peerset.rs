6a83d3cb2bcdad6c3282b42d757bc0f9c7b03dc2
Aaro Altonen <aaro.altonen@parity.io>
2023-09-26 00:10:26 +0300

    Add stub implementation for `Peerset`

--- substrate/client/network/src/litep2p/peerset.rs
+++ substrate/client/network/src/litep2p/peerset.rs
@@ -18,6 +18,124 @@
 
 //! `Peerset` implementation for `litep2p`.
 
-pub struct Litep2pPeerset {}
+use crate::ProtocolName;
 
-impl Litep2pPeerset {}
+use futures::{channel::mpsc::UnboundedReceiver, Stream, StreamExt};
+
+use sc_network_types::PeerId;
+use sc_utils::mpsc::{tracing_unbounded, TracingUnboundedSender};
+
+use std::{
+	collections::HashSet,
+	pin::Pin,
+	task::{Context, Poll},
+};
+
+/// Logging target for the file.
+const LOG_TARGET: &str = "sub-libp2p";
+
+/// Commands emitted by [`Peerset`] to the notification protocol.
+pub enum PeersetCommand {
+	/// Open substream to peer.
+	OpenSubstream {
+		/// Peer Id.
+		peer: PeerId,
+	},
+
+	/// Close substream to peer.
+	CloseSubstream {
+		/// Peer ID.
+		peer: PeerId,
+	},
+
+	/// Set current reserved peer set.
+	///
+	/// This command removes all reserved peers that are not in `peers`.
+	SetReservedPeers {
+		/// New seserved peer set.
+		peers: HashSet<PeerId>,
+	},
+
+	/// Add one or more reserved peers.
+	///
+	/// This command doesn't remove any reserved peers but only add new peers.
+	AddReservePeers {
+		/// Reserved peers to add.
+		peers: HashSet<PeerId>,
+	},
+
+	/// Remove reserved peers.
+	RemoveReservedPeers {
+		/// Reserved peers to remove.
+		peers: HashSet<PeerId>,
+	},
+
+	/// Set reserved-only mode to true/false.
+	SetReservedOnly {
+		/// Should the protocol only accept/establish connections to reserved peers.
+		reserved_only: bool,
+	},
+}
+
+/// `Peerset` implementation.
+///
+/// `Peerset` allows other subsystems of the blockchain to modify the connection state
+/// of the notification protocol by adding and removing reserved peers.
+///
+/// `Peerset` is also responsible for maintaining the desired amount of peers the protocol is
+/// connected to by establishing outbound connections and accepting/rejecting inbound connections.
+pub struct Peerset {
+	/// Protocol name.
+	protocol: ProtocolName,
+
+	/// RX channel for receiving commands.
+	cmd_rx: UnboundedReceiver<PeersetCommand>,
+
+	/// Maximum number of outbound peers.
+	max_out: usize,
+
+	/// Current number of outbound peers.
+	num_out: usize,
+
+	/// Maximum number of inbound peers.
+	max_in: usize,
+
+	/// Current number of inbound peers.
+	num_in: usize,
+
+	/// Current reserved peer set.
+	reserved_peers: HashSet<PeerId>,
+}
+
+impl Peerset {
+	/// Create new [`Peerset`].
+	pub fn new(
+		protocol: ProtocolName,
+		max_out: usize,
+		max_in: usize,
+		reserved_peers: HashSet<PeerId>,
+	) -> (Self, TracingUnboundedSender<PeersetCommand>) {
+		let (cmd_tx, cmd_rx) = tracing_unbounded(&format!("mpsc-peerset-{protocol}"), 100_000);
+
+		(
+			Self {
+				protocol,
+				max_out,
+				num_out: 0usize,
+				max_in,
+				num_in: 0usize,
+				reserved_peers,
+				cmd_rx,
+			},
+			cmd_tx,
+		)
+	}
+}
+
+impl Stream for Peerset {
+	type Item = PeersetCommand;
+
+	fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+		self.cmd_rx.next()
+	}
+}
